3ffe7397e39bece3c30098d2c6e91676fd97f488
Vladislav <vladvavula@gmail.com>
2023-02-25 09:49:31 +0300

    move firebaseApi

---
+++ src/api/firebaseApi.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDwayUijco-PBi9mGyTkEIfZhSqf_ekNfg',
+  authDomain: 'watch-allgather.firebaseapp.com',
+  projectId: 'watch-allgather',
+  storageBucket: 'watch-allgather.appspot.com',
+  messagingSenderId: '740190112818',
+  appId: '1:740190112818:web:19f85343df6ef66d11802b'
+};
+
+const app = initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import App from './App';
 import {store} from './store';
-import './firebase';
+import './api/firebaseApi';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
